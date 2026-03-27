b957b3e14839e821f2933c866d2f299fe6ded04e
Vinan <vinanjayasinghe3@gmail.com>
2026-03-27 21:16:46 +0530

    Fix ecosystem paths for cloud

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,21 +5,21 @@ module.exports = {
       script: "java",
       args: "-jar ./FileShare-Platform-Config-Server/target/config-server-1.0.0.jar",
       log_file: "./logs/config-server.log",
-      restart_delay: 5000,
+      restart_delay: 5000
     },
     {
       name: "service-registry",
       script: "java",
       args: "-jar ./FileShare-Platform-Service-Registry/target/service-registry-1.0.0.jar",
       log_file: "./logs/service-registry.log",
-      restart_delay: 5000,
+      restart_delay: 5000
     },
     {
       name: "api-gateway",
       script: "java",
       args: "-jar ./FileShare-Platform-Api-Gateway/target/api-gateway-1.0.0.jar",
       log_file: "./logs/api-gateway.log",
-      restart_delay: 5000,
+      restart_delay: 5000
     }
   ]
 }
