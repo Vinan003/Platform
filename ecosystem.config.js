5807526f08db4dcb6736e740a353cdab6f182bc5
Vinan <vinanjayasinghe3@gmail.com>
2026-03-27 19:56:25 +0530

    chore: Update PM2 configurations to use `out_file`, add `error_file`, enable `autorestart`, and adjust `restart_delay` for all services.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,21 +4,28 @@ module.exports = {
             name: "config-server",
             script: "java",
             args: "-jar ./FileShare-Platform-Config-Server/target/config-server-1.0.0.jar",
-            log_file: "./logs/config-server.log",
+            out_file: "./logs/config-server.log",
+            error_file: "./logs/config-server-error.log",
+            autorestart: true,
+            restart_delay: 5000
         },
         {
             name: "service-registry",
             script: "java",
             args: "-jar ./FileShare-Platform-Service-Registry/target/service-registry-1.0.0.jar",
-            log_file: "./logs/service-registry.log",
-            restart_delay: 5000,
+            out_file: "./logs/service-registry.log",
+            error_file: "./logs/service-registry-error.log",
+            autorestart: true,
+            restart_delay: 10000 
         },
         {
             name: "api-gateway",
             script: "java",
             args: "-jar ./FileShare-Platform-Api-Gateway/target/api-gateway-1.0.0.jar",
-            log_file: "./logs/api-gateway.log",
-            restart_delay: 5000,
+            out_file: "./logs/api-gateway.log",
+            error_file: "./logs/api-gateway-error.log",
+            autorestart: true,
+            restart_delay: 15000 
         }
     ]
 }
